e4dcb675b4dad5b573c9aa17fbd432e8ec1fee10
blockaj <ajblock@gmail.com>
2014-02-19 11:15:54 -0500

    Added function checkAuth() and /register post.

---
+++ Node_Server/app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var MongoDB = require("MongoDB");
+var mongoose = require("mongoose");
+
+app.post("/register", function(req, res){
+	
+});
+
+function checkAuth() {
+	
+}
+console.log("Server running on port: 3000");
+app.listen(3000);
